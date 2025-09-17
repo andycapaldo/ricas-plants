b4ad18a195663a40e73b0beaf91929380598313c
Andrew Capaldo <andycapaldo2@gmail.com>
2025-09-17 16:28:43 -0600

    Initial commit

---
+++ src/pages/auth/SignInPage.jsx
@@ -0,0 +1,8 @@
+const SignInPage = () => {
+    return <div className="font-playfair text-blue-500">
+        sign in!
+        <i className='fa-solid fa-face-smile'></i>
+    </div>
+};
+
+export default SignInPage;
---
+++ src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,7 @@
+const SignUpPage = () => {
+    return <div className="font-lato text-green-500">
+        sign up!
+    </div>
+};
+
+export default SignUpPage;
